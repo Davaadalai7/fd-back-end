47b73f36e15abb627db5627a639d5f4ad69a8bfe
24LP4621 24LP4621 <24LP4621@G301-CC0210.local>
2025-03-27 18:08:27 +0800

    hhh

--- src/models/category-model.js
+++ src/models/category-model.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const categorysSchema = new mongoose.Schema({
   categoryName: { type: String, required: true },
-  createdAt: { type: Date, required: true },
-  updatedAt: { type: Date, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  updatedAt: { type: Date, required: true, default: Date.now },
 });
 
 export const Category = mongoose.model("category", categorysSchema);
