7d10cd0a1dc29aae3b570d62fb1340c015807c99
24LP4621 24LP4621 <24LP4621@G301-CC0210.local>
2025-04-02 22:00:56 +0800

    aaa

--- src/controller/category/postCategoryController.js
+++ src/controller/category/postCategoryController.js
@@ -1,9 +1,9 @@
 import { Category } from "../../models/category-model.js";
 
-export const PostCatergoryController = (req, res) => {
+export const PostCatergoryController = async (req, res) => {
   const { categoryName } = req.body;
   try {
-    const category = Category.create({ categoryName: categoryName });
+    const category = await Category.create({ categoryName: categoryName });
     res
       .status(200)
       .json({ success: true, message: "category created", data: category });
